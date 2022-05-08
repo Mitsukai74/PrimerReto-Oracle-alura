f47fa84ca51302f1df320f934cff92e186afe98e
Mitsukai74 <ansaanva@gmail.com>
2022-05-08 12:27:39 -0500

    se agrega funcionalidad de los botones

--- scripting.js
+++ scripting.js
@@ -1,5 +1,31 @@
 const entrada=document.querySelector(".input_text1");
-function botonEncriptar(){
-    
+const salida=document.querySelector(".input_text2");
 
+function botonEncriptar(){
+    const txtEncr = encrip(entrada.value);
+    salida.value = txtEncr;
+}
+function encrip(strPara){
+    let patron = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    strPara = strPara.toLowerCase();
+
+    for(let i=0;i<patron.length;i++){
+        if(strPara.includes(patron[i][0])){
+            strPara = strPara.replaceAll(patron[i][0],patron[i][1]);
+        }
+    }
+    return strPara;
+}
+function botonDesencriptar(){
+    const txtParaDescrepitar=descencrip(salida.value);
+    entrada.value=txtParaDescrepitar;
+}
+function descencrip(strParaDesencip){
+    let patron1 = [["enter","e"],["imes","i"],["ai","a"],["ober","o"],["ufat","u"]];
+    for(let i=0;i<patron1.length;i++){
+        if(strParaDesencip.includes(patron1[i][0])){
+            strParaDesencip = strParaDesencip.replaceAll(patron1[i][0],patron1[i][1])
+        }
+    }
+return strParaDesencip;
 }
